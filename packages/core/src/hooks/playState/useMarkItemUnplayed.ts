0b7ccb1026abf9aafb945a13b973f1fdf70379d4
Isaac Eaton <iseaton@users.noreply.github.com>
2026-08-17 10:52:54 +0200

    Update useMarkItemUnplayed.ts

--- packages/core/src/hooks/playState/useMarkItemUnplayed.ts
+++ packages/core/src/hooks/playState/useMarkItemUnplayed.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { getPlaystateApi } from '@jellyfin/sdk/lib/utils/api/playstate-api';
 import { getApi } from '../../api/getApi';
+import { invalidateItemCache } from '../../api/getItemCached';
 
 interface MarkUnplayedInput {
     itemId: string;
@@ -23,6 +24,8 @@ export function useMarkItemUnplayed() {
             });
         },
         onSuccess: (_, { itemId, userId }) => {
+            invalidateItemCache(itemId);
+
             queryClient.invalidateQueries({
                 queryKey: ['itemPlayState', userId, itemId],
             });
